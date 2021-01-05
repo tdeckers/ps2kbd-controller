a69038dc4280dff7c419182f6fe1e707eb1a4fe0
Tom Deckers <tom@ducbase.com>
2021-01-05 21:31:21 +0100

    Finalize port.
    * Last functionality ported
    * reorganized some code in functions to reuse (mimin ASM labels)

--- README.md
+++ README.md
@@ -10,21 +10,39 @@ This is the first real PIC project, so code might be crappy, but as of the
 first checked in version it seems to be working.
 
 Inspiration for the code came from <http://sbc.rictor.org/pckbavr.html>.  That
-code was written in assembler. I'm not that brave. I ported it to C.
+code was written in assembler. I'm not that brave. I ported it to C without too
+much optimizations, so that's why the code is a bit clumsy.  I might get around
+to optimize it later, but it works for now.
+
+## Hardware setup
+
+The PS/2 keyboard is connected as follows:
+* RA0 - keyboard clock
+* RA1 - keyboard data
+* Connect keyboard VCC and GND.
+
+This controller is intended to be interfaced to the 6502 through a 
+6522 VIA.  
+* 8 bits of port B are to be connected to the VIA ports data lines.
+* RA2 - to VIA CA1 (data ready)
+* RA3 - from VIA CA2 (data ack)
+
+When the controller receives a new character from the keyboard, it sets the
+data bits on port B. It then asserts the data_ready signal.  The CPU can then
+read the character through the VIA and that will automatically assert the 
+data_ack signal.
 
 This project has been developed and tested with a PIC16F1826.
 
 ## Features
 
-* Receive bytes from keyboard on key press
+* Receive bytes from keyboard on key press and presents the byte to the VIA.
 * Send commands to keyboard (e.g. control LEDs)
+* Interfaces with a 6502 through a VIA port.
 
-Current version does the night rider effect on the LEDs.
-
-Next up: 
-* actual 6502 interfacing
-* clean up variables, etc..
-
+Future:
+* watchdog reset in case the controller gets stuck in any of the loops waiting
+  for a given state.
 
 ## Development notes
 
--- main.c
+++ main.c
@@ -77,17 +77,10 @@ int cntr;
 
 byte read_bit;
 
-void blink_ok(int times) {
-    static byte i;
-    for (i = 0; i < times; i++) {
-        DEBUG = 1;
-        __delay_ms(100);
-        DEBUG = 0;
-        __delay_ms(100);
-    }
-}
 
 void setup(void) {
+    di();   // disable interrupts(XC8 code)
+        
     OSCCON = 0b01110000;  // 8 Mhz
     // configure I/O direction: 1=input / 0=output
     TRISB = 0; // port A all output (to host)
@@ -100,14 +93,14 @@ void setup(void) {
     DATAACK_DIR = DIR_IN;
     RST_DIR = DIR_IN;
     DEBUG_DIR = DIR_OUT;
-    DEBUG = 0;
+    DEBUG = 0;  
 }
 
 /*
  * https://stackoverflow.com/questions/21617970/how-to-check-if-value-has-even-parity-of-bits-or-odd
  * but modified
  */
-byte check_parity(byte data, byte parity) {
+static byte check_parity(byte data, byte parity) {
     static byte count, i;
     count = 0;
     for (i = 0; i < 8; i++) {
@@ -129,7 +122,7 @@ byte check_parity(byte data, byte parity) {
  * Reverse the bits in a byte.
  * https://stackoverflow.com/questions/2602823/in-c-c-whats-the-simplest-way-to-reverse-the-order-of-bits-in-a-byte
  */
-byte reverse(byte b) {
+static byte reverse(byte b) {
     b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
     b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
     b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
@@ -140,7 +133,7 @@ byte reverse(byte b) {
  * process single clock pulse.  On the falling edge, capture data bit and 
  * return. This bit will be ignored for send().
  */
-byte highLow() {
+static byte highLow() {
     while (!KBCLK) {
     } // wait while clock is low
     while (KBCLK) {
@@ -151,7 +144,7 @@ byte highLow() {
 /*
  * disable keyboard from sending.  Pulling the clock low inhibits the keyboard.
  */
-void disable() {
+static void disable() {
     KBCLK_DIR = DIR_OUT;
     KBCLK = 0;
 }
@@ -159,7 +152,7 @@ void disable() {
 /*
  * enable keyboard for receiving.  Ensure clock is high first.
  */
-void enable() {
+static void enable() {
     KBCLK_DIR = DIR_OUT;
     KBCLK = 1;
     KBCLK_DIR = DIR_IN;    
@@ -168,7 +161,7 @@ void enable() {
 /*
  * get a single byte from the keyboard.
  */
-byte get() {
+static byte get() {
     static byte stop;
     static byte i;
     
@@ -205,7 +198,7 @@ byte get() {
 /*
  * send a single byte to the keyboard.
  */
-void send(byte d) {
+static void send(byte d) {
     static byte bits[8];
     static byte i;
     // prepare the data.  We don't want to do this during the pulses.
@@ -245,23 +238,75 @@ void send(byte d) {
     // TODO check read_bit = 0 (ack)
 }
 
-void initkbd() {
-    
+static void setleds() {
+    static int done;
+    done = -1;
+    while (done < 0) {          // as long as we didn't get an ack...
+        send(0xED);             // send command indication
+        data = get();           // get response
+        if (data == 0xFA) {     // if ack...
+            send( special & 0x07 ) ;  // ... send led data ... 
+            done = 1;           // and set done.            
+        }        
+    }
 }
 
-byte lookup(byte d) {
+static void initkbd() {
+    special |= 1 << NUMLOCK;
+    static int done;
+    done = -1;
+    
+    while (done < 0) {
+        send(0xFF);             // send command reset
+        data = get();           // get response
+        if (data == 0xFA) {     // if ack ..
+            data = get();       // 
+            if (data == 0xAA) { // get ok.
+                done = 1;       // we're done.
+            }            
+        }
+
+    }    
+    
+    setleds();
+}
+
+static byte lookup(byte d) {
+    static byte retVal;
     if (special & (1<<SHIFT) || special & (1<<EXTSH)) {
-        return keymap_shifted[d];
+        retVal = keymap_shifted[d];
     } else {
-        return keymap[d];
+        
+        retVal = keymap[d];
+    }    
+    
+    // process numpad keys when numlock is on.
+    if (d >= 0x69 && d <= 0x7D          // if numpad            
+            && d != 0x78) {             // not F11 
+        if (! (special & (1 << NUMLOCK)) ) {    // if numlock off, 
+            retVal = keymap_shifted[d];         // return shifted values.
+        }
     }
     
+    if (special & (1 << CONTROL)) {     // control key active?
+        // not sure what to do here.
+    }
+    
+    if (special & (1 << RLSE)) {        // release code
+        retVal = 0x00;                  // clear any code that got set
+    }
+    
+    if ((special & (1 << CAPSLOCK))     // caps lock on?
+            && retVal >= 0x61 && retVal <= 0x7A ) {     // and character?
+        retVal &= 0xDF;                 // to upper case.
+    }
+    return retVal;
 }
 
 /*
  * process key release code.
  */
-void rlsproc(int * retVal, byte * data) {
+static void rlsproc(int * retVal, byte * data) {
     special &= ~(1<<RLSE);  // clear release flag
     if (*data == 0x11) {     // alt released?
         *retVal = 0xE0;
@@ -274,65 +319,98 @@ void rlsproc(int * retVal, byte * data) {
     }    
 }
 
-byte kbdinput() {
+static void extproc(int * retVal, byte * data) {
+    special &= ~(1 << EXT);         // clear the ext flag
+    if (*data == 0x12) {
+        special |= 1 << EXTSH;      // set ext shift flag.
+    } else if (*data == 0x7C) {      // E07C - print screen?
+        special &= ~(1<<EXTSH);     // turn off ext shift flag?
+        *retVal = lookup(0x0F);
+    } else if (*data == 0x4A) {      // keypad div key
+        *retVal = 0x2F;
+    } else if (*data == 0xF0) {      // was it a release code?
+        *data = get();
+        if (*data == 0x12) {         // was it ext shift?
+            special &= ~(1<<EXTSH);
+        }
+        rlsproc(retVal, data);
+    } else if (*data == 0x7E) {        // ctrl-brk key?
+        *retVal = 0x02;
+    } else if (*data > 0x68 ) {  // if key is keypad key
+        *retVal = lookup(*data);
+    }    
+}
+
+/*
+ * main tests for functional key presses.
+ * 
+ * ref: exttst
+ */
+static void testkeys(int * retVal, byte * data) {
+    if (*data == 0x14) {                    // CTRL key
+        special |= 1 << CONTROL;
+    } else if (*data == 0x58) {             // caps lock
+        if ( special & (1 << CAPSLOCK) ) {  // if 1
+            special &= ~(1<<CAPSLOCK);      // clear it.
+        } else {                            // else set it.
+            special |= 1 << CAPSLOCK;
+        }
+        setleds();
+        
+    } else if (*data == 0x77) {             // num lock
+        if ( special & (1 << NUMLOCK) ) {   // if 1
+            special &= ~(1<<NUMLOCK);       // clear it.
+        } else {                            // else set it.
+            special |= 1 << NUMLOCK;
+        }
+        setleds();
+    } else if (*data == 0x7E) {             // scroll lock
+        if ( special & (1 << SCRLLOCK) ) {  // if 1...
+            special &= ~(1<<SCRLLOCK);      // clear it,
+        } else {                            // else set it.
+            special |= 1 << SCRLLOCK;
+        }
+        setleds();
+        *retVal = lookup(0x7E);     // setleds might have changed *data!
+        
+    } else if (*data < 0x80) {
+        *retVal = lookup(*data);
+            
+    } else if (*data == 0x83) {     // F7 key?
+        *retVal = lookup(0x02);     // lookup relocated value.
+        
+    } else if (*data == 0xE0) {  // ext code
+        special |= 1 << EXT;
+    
+    } else if (*data == 0xE1) {     // BRK key
+        // flush 7 break key codes.
+        get(); get(); get(); get(); get(); get(); get();
+        *retVal = 0x03;
+        
+    } else if (*data == 0xF0) {  // key release code?
+        special |= 1 << RLSE;   // set release state
+                                   // get the next byte;
+    }
+}
+
+static byte kbdinput() {
     static int retVal;
     retVal = -1;
     while(retVal == -1) {
         data = get();
         if (special & 1 << RLSE) {  // if release flag set
-            // reference: Rlsproc
             rlsproc(&retVal, &data);
+            
         } else if (special & 1 << EXT) {    // is ext flag active?
-            // reference: Extproc
-            special &= ~(1 << EXT);         // clear the ext flag
-            if (data == 0x12) {
-                special |= 1 << EXTSH;      // set ext shift flag.
-                continue;
-            } else if (data == 0x7C) {      // E07C - print screen?
-                special &= ~(1<<EXTSH);     // turn off ext shift flag?
-                retVal = lookup(0x0F);
-            } else if (data == 0x4A) {      // keypad div key
-                retVal = 0x2F;
-            } else if (data == 0xF0) {      // was it a release code?
-                data = get();
-                if (data == 0x12) {         // was it ext shift?
-                    special &= ~(1<<EXTSH);
-                    continue;
-                }
-                special &= ~(1<<RLSE);  // clear release flag
-                if (data == 0x11) {     // alt released?
-                    retVal = 0xE0;
-                    continue;
-                } else if (data == 0x12 || data == 0x59) {  // shift released?
-                    special &= ~(1<<SHIFT);
-                    continue;
-                } else if (data == 0x14) {  // ctrl released?
-                    special &= ~(1<<CONTROL);
-                    continue;
-                } else {
-                    continue;   // else don't care, get next key.
-                }                                
-            } else if (data == 0x7E) {        // ctrl-brk key?
-                retVal = 0x02;
-                continue;
-            } else if (data > 0x68 ) {  // if key is keypad key
-                retVal = lookup(data);
-            }
+            extproc(&retVal, &data);
+            
         } else if (data == 0x12 || data == 0x59) {
             special |= 1 << SHIFT;  // set shift state                    
-            //retVal = 0;             // no further action on shift.
-            continue;
-        } else if (data < 0x80) {
-            retVal = lookup(data);   
-        } else if (data == 0xE0) {  // ext code
-            special |= 1 << EXT;
-            continue;
-        } else if (data == 0xF0) {  // key release code?
-            special |= 1 << RLSE;   // set release state
-            continue;               // get the next byte;
+                                    // no further action on shift.
         } else {
-            continue;                // no valid key, get next
-        };
+            testkeys(&retVal, &data);
+        }
+        // as long as we have no valid key, get next (loop around)
 
     }
     
@@ -343,9 +421,6 @@ void main(void) {
     setup();
     
     initkbd();
-        
-    __delay_ms(2000);
-    blink_ok(2);    // debug.
     
     while (1) {
         data = kbdinput();
@@ -355,21 +430,7 @@ void main(void) {
         }
         // got ACK.
         DATARDY = 1;
-    }
-    
-    byte leds[] = {2, 6, 5, 1, 0, 1, 5, 6, 2, 0};
-    int cnt = 0;
-    while (1) {
-        send(0xED);
-        data = get();
-        if (data == 0xFA) {
-            send(leds[cnt++]);
-        }        
-        if (cnt >= (sizeof leds / sizeof leds[0])) {
-            cnt = 0;
-        }
-        __delay_ms(100); // 1 Second Delay
-    }
+    }    
 
     return;
 }
